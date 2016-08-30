cb5dd6843ececf90e698daa8e40c62d7f1e3b932
User <User@USER-PC>
2016-08-30 14:27:18 +0800

    08301427

---
+++ 0829_Test/TestLoop.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="C:\Work\Visual Studio\August\0829_Test\0829_Test\0829_Test\jquery-2.2.4.js" />
+
+
+var MyAry = ["A", "B", "C", "D"];
+
+var MyAry2 = new Array("AA", "BB", "CC", "DD");
+
+var Join = MyAry.join("--");
+
+console.info(Join);
+
+console.info(MyAry2);
